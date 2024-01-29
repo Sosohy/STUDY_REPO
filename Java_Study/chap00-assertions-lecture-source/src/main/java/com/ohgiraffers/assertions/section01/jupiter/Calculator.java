394663cfa668f8f468be6b3d0f824dfec85b2e9a
hso <sh07202@naver.com>
2024-01-29 14:03:43 +0900

    [UPDATE] java assertions

---
+++ Java_Study/chap00-assertions-lecture-source/src/main/java/com/ohgiraffers/assertions/section01/jupiter/Calculator.java
@@ -0,0 +1,7 @@
+package com.ohgiraffers.assertions.section01.jupiter;
+
+public class Calculator {
+    public int plusTwoNums(int firstNum, int secondNum) {
+        return firstNum + secondNum;
+    }
+}
