1ab1a8818497083438c77e7a12f80328e42735e9
hso <sh07202@naver.com>
2024-02-01 15:37:42 +0900

    [UPDATE] java algorithm - Tree, DP comment update

--- Java_Study/chap17-algorithm-lecture-source/src/main/java/com/ohgiraffers/chap06/section01/dp/Application1.java
+++ Java_Study/chap17-algorithm-lecture-source/src/main/java/com/ohgiraffers/chap06/section01/dp/Application1.java
@@ -1,5 +1,10 @@
 package com.ohgiraffers.chap06.section01.dp;
 
+/* 수업목표. 동적계획법(DynamicProgramming) 알고리즘을 활용하는 예제를 이해할 수 있다. */
+/* 필기
+ *  복잡한 전체 문제를 작은 부분 문제에 대한 최적의 해결방법을 고민하여 해결하는 알고리즘
+ *  규칙이 보이는 구간들에 대한 점화식을 세워 작은 문제들을 해결하면 전체 문제의 최적의 값이 나오게 된다.
+* */
 public class Application1 {
 
     public static int solution(int N) throws Exception{
