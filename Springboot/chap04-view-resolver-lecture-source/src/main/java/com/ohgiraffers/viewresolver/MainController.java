cb115c1d7a66a97c4bdae0ce0d735d064986fa4d
hso <sh07202@naver.com>
2024-02-16 17:10:40 +0900

    [UPDATE] springboot resolver, exception

--- Springboot/chap04-view-resolver-lecture-source/src/main/java/com/ohgiraffers/viewresolver/MainController.java
+++ Springboot/chap04-view-resolver-lecture-source/src/main/java/com/ohgiraffers/viewresolver/MainController.java
@@ -11,7 +11,7 @@ public class MainController {
      *  ViewResolver(뷰 리졸버) 인터페이스를 구현한 ThymeleafViewResolver가 현재 처리하게 된다.
      *  접두사(prefix) : resource/templates/
      *  접미사(suffix) : .html
-     *  핸들러 메소드가 반환하는 String 값 앞 뒤에 접두사와 접미사가 붙엉 view를 찾게 된다.
+     *  핸들러 메소드가 반환하는 String 값 앞 뒤에 접두사와 접미사가 붙어 view를 찾게 된다.
     * */
     @RequestMapping(value={"/", "/main"})
     public String main(){
