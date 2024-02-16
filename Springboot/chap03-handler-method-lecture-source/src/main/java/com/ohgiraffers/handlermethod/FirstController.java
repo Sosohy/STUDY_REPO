6d3dc22a863934e07ef9bb87e99d1690e82cf2f7
hso <sh07202@naver.com>
2024-02-16 09:50:00 +0900

    [UPDATE] springboot @RequestParam

--- Springboot/chap03-handler-method-lecture-source/src/main/java/com/ohgiraffers/handlermethod/FirstController.java
+++ Springboot/chap03-handler-method-lecture-source/src/main/java/com/ohgiraffers/handlermethod/FirstController.java
@@ -7,7 +7,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.request.WebRequest;
+
+import java.util.Map;
 
 @Controller
 
@@ -38,4 +41,42 @@ public class FirstController {
         return "first/messagePrinter";
     }
 
+    @GetMapping("modify")
+    public void modify(){}
+
+    /* 설명
+     *  request의 parameter로 넘어오는 값들의 key값과 일치하는 변수명을 작성하고
+     *  @RequestParam 어노테이션을 적용하면 알아서 값을 꺼내고 해당 매개변수의 자료형에 맞게 변환까지 해준다.
+     *  ** @RequestParam 어노테이션은 생략도 가능하다.
+     *  (스프링부터는 컨트롤러의 파싱(parsing)작업이 간소화 된다는 것을 알 수 있다.)
+     *  ** default value 설정 가능
+    * */
+
+    /* 설명
+     *  @RequestParam의 속성들
+     *  1. defaultValue : 사용자의 입력 값이 없거나("") 아니면 request의 parameter 키 값과 일치하지 않는 매개변수명 사용 시 매개변수가 가질 기본 default값을 작성한다.
+     *  2. name : request parameter의 키 값과 다른 매개변수 명을 사용하고 싶을 때 request parameter의 키 값을 작성한다.
+    * */
+    @PostMapping("modify")
+    public String modifyMenu(Model model,
+                             @RequestParam(defaultValue = "디폴트", name= "name") String modifyName,
+                             @RequestParam(defaultValue = "0") int modifyPrice){
+
+        String msg = modifyName + "의 가격을 " + modifyPrice + "원으로 변경";
+        model.addAttribute("message", msg);
+
+        return "first/messagePrinter";
+    }
+
+    @PostMapping("modify2")
+    public String modifyMenu(Model model, @RequestParam Map<String, String> parameter){
+
+        String modifyName = parameter.get("name2");
+        int modifyPrice = Integer.valueOf(parameter.get("modifyPrice2"));
+
+        String msg = modifyName + "의 가격을 " + modifyPrice + "원으로 변경";
+        model.addAttribute("message", msg);
+
+        return "first/messagePrinter";
+    }
 }
