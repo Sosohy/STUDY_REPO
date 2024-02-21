6da9ebc8ea643f2b22c8606f037928ea11818d0f
hso <sh07202@naver.com>
2024-02-21 15:39:34 +0900

    [UPDATE] MyBatis dynamicSql foreach/trim

--- Backend/MyBatis/chap03-dynamic-sql-lecture-source/src/main/java/com/ohgiraffers/section01/xml/MenuService.java
+++ Backend/MyBatis/chap03-dynamic-sql-lecture-source/src/main/java/com/ohgiraffers/section01/xml/MenuService.java
@@ -64,4 +64,49 @@ public class MenuService {
             System.out.println("DB와 연동 실패 또는 검색 결과 없음");
         sqlSession.close();
     }
+
+    public void searchMenuByCodeOrSearchAll(SearchCriteria searchCriteria) {
+
+        SqlSession sqlSession = getSqlSession();
+        MenuMapper menuMapper = sqlSession.getMapper(MenuMapper.class);
+
+        List<MenuDTO> menus = menuMapper.searchMenuByCodeOrSearchAll(searchCriteria);
+
+        if(menus != null && !menus.isEmpty())
+            menus.forEach(System.out::println);
+        else
+            System.out.println("DB와 연동 실패 또는 검색 결과 없음");
+        sqlSession.close();
+    }
+
+    public void searchMenuByNameOrCategory(Map<String, Object> criteria) {
+
+        SqlSession sqlSession = getSqlSession();
+        MenuMapper menuMapper = sqlSession.getMapper(MenuMapper.class);
+
+        List<MenuDTO> menus = menuMapper.searchMenuByNameOrCategory(criteria);
+
+        if(menus != null && !menus.isEmpty())
+            menus.forEach(System.out::println);
+        else
+            System.out.println("DB와 연동 실패 또는 검색 결과 없음");
+        sqlSession.close();
+
+    }
+
+    public void modifyMenu(Map<String, Object> criteria) {
+
+        SqlSession sqlSession = getSqlSession();
+        MenuMapper menuMapper = sqlSession.getMapper(MenuMapper.class);
+
+        int result = menuMapper.updateMenu(criteria);
+        if(result > 0) {
+            System.out.println("정보 수정 성공");
+            sqlSession.commit();
+        }else {
+            System.out.println("정보 수정 실패");
+            sqlSession.rollback();
+        }
+        sqlSession.close();
+    }
 }
