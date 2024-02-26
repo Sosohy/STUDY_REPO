dff01fd9aad4b6a0209730287b7a87b7ca16587e
hso <sh07202@naver.com>
2024-02-26 14:07:05 +0900

    [UPDATE] JPA auto DDL

--- Backend/JPA/chap01-persistence-context-lecture-source/src/test/java/com/ohgiraffers/section01/A_EntityManagerLifeCycleTests.java
+++ Backend/JPA/chap01-persistence-context-lecture-source/src/test/java/com/ohgiraffers/section01/A_EntityManagerLifeCycleTests.java
@@ -12,29 +12,34 @@ public class A_EntityManagerLifeCycleTests {
     private EntityManager entityManager;
 
     @BeforeAll
-    public static void initFactory(){
+    public static void initFactory() {
         entityManagerFactory = Persistence.createEntityManagerFactory("jpatest");
     }
 
     @BeforeEach
-    public void initManager(){
+    public void initManager() {
         entityManager = entityManagerFactory.createEntityManager();
     }
 
     @Test
-    public void 엔티티_매니저_팩토리와_엔티티_매니저_생명주기_확인(){
-        System.out.println("entityManagerFactory.hashCode : " + entityManagerFactory.hashCode());
-        System.out.println("entityManager.hashCode : " + entityManager.hashCode());
+    public void 엔티티_매니저_팩토리와_엔티티_매니저_생명주기_확인() {
+        System.out.println("entityManagerFactory.hashCode: " + entityManagerFactory.hashCode());
+        System.out.println("entityManager.hashCode: " + entityManager.hashCode());
+    }
+
+    @Test
+    public void 엔티티_매니저_팩토리와_엔티티_매니저_생명주기_확인2() {
+        System.out.println("entityManagerFactory.hashCode: " + entityManagerFactory.hashCode());
+        System.out.println("entityManager.hashCode: " + entityManager.hashCode());
     }
 
     @AfterAll
-    public static void closeFactory(){
+    public static void closeFactory() {
         entityManagerFactory.close();
     }
 
     @AfterEach
-    public void closeManager(){
+    public void closeManager() {
         entityManager.close();
     }
-
 }
