ad5dd11291b4bafe5045f15f077fd106d70d2764
hso <sh07202@naver.com>
2024-03-05 12:23:23 +0900

    [UPDATE] login - security

--- MSA/chap04-01-user-service-lecture-source/src/main/java/com/ohgiraffers/userservice/repository/UserRepository.java
+++ MSA/chap04-01-user-service-lecture-source/src/main/java/com/ohgiraffers/userservice/repository/UserRepository.java
@@ -2,6 +2,9 @@ package com.ohgiraffers.userservice.repository;
 
 import com.ohgiraffers.userservice.aggregate.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    UserEntity findByEmail(String email);
 }
