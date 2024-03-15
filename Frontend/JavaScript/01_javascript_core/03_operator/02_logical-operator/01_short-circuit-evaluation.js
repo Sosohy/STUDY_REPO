4e49d81bff6732104ba0c78647e7ee62cc500d46
hso <sh07202@naver.com>
2024-03-15 12:39:16 +0900

    [UPDATE] javaScript

---
+++ Frontend/JavaScript/01_javascript_core/03_operator/02_logical-operator/01_short-circuit-evaluation.js
@@ -0,0 +1,16 @@
+/* 01. short-circuit-evaluation(논리 연산자) */
+
+/* 표현식을 평가하는 도중 평가 결과가 확정된 경우 나머지 평가 과정을 생략하는 것 */
+
+/* 1. OR의 경우 */
+
+// 'apple', 'banana'은 비어있지 않은 문자열 -> Truthy -> true||true인 셈이다. 
+console.log('apple' || 'banana');   // apple -> 첫번째 true가 판단 근거니까 apple이 나옴
+console.log('' || 'banana')         // banana
+console.log('apple' || false)       // apple 
+console.log('-----------');
+
+/* AND의 경우 */
+console.log('apple' && 'banana');   // banana
+console.log(false && 'banana');     // false
+console.log('apple' && false);      // false
