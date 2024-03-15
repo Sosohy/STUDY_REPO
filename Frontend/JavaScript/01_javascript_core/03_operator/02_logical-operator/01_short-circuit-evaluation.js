e4ec02dadfb0537e37202565b6a41f0e495fa585
hso <sh07202@naver.com>
2024-03-15 12:43:11 +0900

    [UPDATE] JavaScript

--- Frontend/JavaScript/01_javascript_core/03_operator/02_logical-operator/01_short-circuit-evaluation.js
+++ Frontend/JavaScript/01_javascript_core/03_operator/02_logical-operator/01_short-circuit-evaluation.js
@@ -14,3 +14,18 @@ console.log('-----------');
 console.log('apple' && 'banana');   // banana
 console.log(false && 'banana');     // false
 console.log('apple' && false);      // false
+console.log('-----------');
+
+var num = 1;
+
+/*
+if(num%2 == 0) {
+    console.log('짝수');
+}else{
+    console.log('홀수');
+}
+*/
+
+/* 단축 평가를 활용하여 같은 결과를 얻을 수 있다. */
+num % 2 == 0 && console.log('짝수');
+num % 2 == 0 || console.log('홀수');
